1a29879530eb13ff86742c0d3154972b7f5a2216
Gabriel Novaes <80960360+Yukitte@users.noreply.github.com>
2025-06-27 17:20:19 -0300

    Delete server directory

--- server/routes/habitRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getHabits, createHabit } = require("../controllers/habitController");
-
-router.get("/:userId", getHabits);
-router.post("/", createHabit);
-
-module.exports = router;
